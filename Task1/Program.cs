80646d1f9da8d09e7aa54d0e89ee066fcef710fb
XeniaLS13 <kseniavagabova@mail.ru>
2023-01-20 02:22:52 +0300

    добавлены файлы с решением заданий

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите элементы: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов > 0: {count}");
